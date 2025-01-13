eda5d60e9810a74c320288957908fb824e1967ac
warluck11 <aditya8kale@gmail.com>
2025-01-13 09:22:00 +0530

    first commit

---
+++ GameStop/src/test/java/com/itvedant/gamestop/GameStopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvedant.gamestop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
